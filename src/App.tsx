d1a5123ac68299bc9c78bf45d6cfdb562cd28719
longdv ph29867 <longdvph29867@gmail.com>
2024-05-19 17:12:02 +0700

    fix config

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
-import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import DetailProduct from "./pages/DetailProduct";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import DetailProduct from "./pages/DetailProduct";
+import { Route, Routes } from "react-router-dom";
 import AccountPage from "./pages/AccountPage/AccountPage";
 import AccountInfomation from "./components/Accout/AccountInfomation";
 import ChangePassword from "./components/Accout/ChangePassword";
@@ -11,11 +9,12 @@ function App() {
   return (
     <div>
       <Routes>
-      <Route path="/" Component={AccountPage}>
-        <Route path="infomation" Component={AccountInfomation}/>
-        <Route path="updatepassword" Component={ChangePassword}/>
-        <Route path="/detail" element={<DetailProduct />} />
-      </Route>
+        <Route path="/" Component={AccountPage}>
+          <Route path="account" Component={AccountInfomation} />
+          <Route path="infomation" Component={AccountInfomation} />
+          <Route path="updatepassword" Component={ChangePassword} />
+          <Route path="detail" element={<DetailProduct />} />
+        </Route>
         {/* Các route khác nếu có */}
       </Routes>
     </div>
