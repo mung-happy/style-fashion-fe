0c181ea25b78005db8582d4cd41f81565fb2a68c
linh <linh0906203@gmail.com>
2024-05-26 22:19:32 +0700

    edit

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,18 @@
-import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import DetailProduct from "./pages/DetailProduct";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import DetailProduct from "./pages/DetailProduct";
 import AccountPage from "./pages/AccountPage/AccountPage";
 import AccountInfomation from "./components/Accout/AccountInfomation";
 import ChangePassword from "./components/Accout/ChangePassword";
-
 function App() {
   return (
     <div>
       <Routes>
-      <Route path="/" Component={AccountPage}>
-        <Route path="infomation" Component={AccountInfomation}/>
-        <Route path="updatepassword" Component={ChangePassword}/>
+        <Route path="/" Component={AccountPage}>
+          <Route path="infomation" Component={AccountInfomation} />
+          <Route path="updatepassword" Component={ChangePassword} />
+        </Route>
         <Route path="/detail" element={<DetailProduct />} />
-      </Route>
         {/* Các route khác nếu có */}
       </Routes>
     </div>
