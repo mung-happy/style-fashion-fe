1136fabfc6c523fcb2c38f3d24b2afce592c1003
hoanganh2092003 <laithehoanganh2092003@gmail.com>
2024-07-14 16:57:47 +0700

    Update layout and color banner

--- src/components/HomeComponent/Banner/Banner.tsx
+++ src/components/HomeComponent/Banner/Banner.tsx
@@ -82,74 +82,74 @@ const Banner = () => {
   ];
   return (
     <>
-      <div className="bg-[#E3FFE6] relative">
-        <img
-          src={imgBannerBg}
-          alt="img-banner-bg"
-          className="absolute top-0 left-0 object-cover w-full h-full"
-        />
-        <Slider {...settings}>
-          {bannerList.map((item, index) => (
-            <div>
-              <div className="relative left-0 top-0 w-full h-full pb-[42%]">
-                <div className="absolute top-0 left-0 w-full h-full">
-                  <div className="container flex justify-end h-full mx-auto">
-                    <img
-                      src={item.img}
-                      alt="img-banner"
-                      className="absolute h-full"
-                      id={`slick-slide-img${index}`}
-                    />
-                    <div
-                      id={`slick-slide-text${index}`}
-                      className="w-full h-full"
-                    >
-                      <div className="flex flex-col items-start justify-center h-full relative z-10">
-                        <span className="text-sm sm:text-xl text-slate-700">
-                          {item.content}
+    <div className="bg-[#ffecf09a] relative banner-slider">
+      <img
+        src={imgBannerBg}
+        alt="img-banner-bg"
+        className="absolute top-0 left-0 object-cover w-full h-full"
+      />
+      <Slider {...settings}>
+        {bannerList.map((item, index) => (
+          <div>
+            <div className="relative left-0 top-0 w-full h-full pb-[42%]">
+              <div className="absolute top-0 left-0 w-full h-full">
+                <div className="container flex justify-end h-full mx-auto">
+                  <img
+                    src={item.img}
+                    alt="img-banner"
+                    className="absolute h-full"
+                    id={`slick-slide-img${index}`}
+                  />
+                  <div
+                    id={`slick-slide-text${index}`}
+                    className="w-full h-full"
+                  >
+                    <div className="flex flex-col items-start justify-center h-full relative z-10">
+                      <span className="text-sm sm:text-xl text-[#6a6a6a]">
+                        {item.content}
+                      </span>
+                      <h1
+                        dangerouslySetInnerHTML={{ __html: item.title }}
+                        className="mt-1 text-xl font-semibold ls:leading-10 sm:mt-2 lg:mt-6 sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl text-[#222]"
+                      ></h1>
+                      <a
+                        className="relative inline-flex items-center justify-center h-auto px-6 py-3 mt-2 text-sm font-medium transition-colors rounded-full shadow-xl sm:mt-4 lg:mt-12 sm:text-base sm:py-5 sm:px-9 bg-[#fe385c] text-white"
+                        href="/"
+                      >
+                        <span>Xem ngay</span>
+                        <span>
+                          <svg
+                            className="w-5 h-5 ml-2.5"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                          >
+                            <path
+                              d="M11.5 21C16.7467 21 21 16.7467 21 11.5C21 6.25329 16.7467 2 11.5 2C6.25329 2 2 6.25329 2 11.5C2 16.7467 6.25329 21 11.5 21Z"
+                              stroke="#fff"
+                              strokeWidth="2"
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                            />
+                            <path
+                              d="M222 222L20 20"
+                              stroke="#fff"
+                              strokeWidth="2"
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                            ></path>
+                          </svg>
                         </span>
-                        <h1
-                          dangerouslySetInnerHTML={{ __html: item.title }}
-                          className="mt-1 text-xl font-semibold ls:leading-10 sm:mt-2 lg:mt-6 sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl text-slate-800"
-                        ></h1>
-                        <a
-                          className="relative inline-flex items-center justify-center h-auto px-6 py-3 mt-2 text-sm font-medium transition-colors rounded-full shadow-xl sm:mt-4 lg:mt-12 sm:text-base sm:py-5 sm:px-9 bg-slate-800 text-slate-50"
-                          href="/"
-                        >
-                          <span>Xem ngay</span>
-                          <span>
-                            <svg
-                              className="w-5 h-5 ml-2.5"
-                              viewBox="0 0 24 24"
-                              fill="none"
-                            >
-                              <path
-                                d="M11.5 21C16.7467 21 21 16.7467 21 11.5C21 6.25329 16.7467 2 11.5 2C6.25329 2 2 6.25329 2 11.5C2 16.7467 6.25329 21 11.5 21Z"
-                                stroke="currentColor"
-                                strokeWidth="1.5"
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                              />
-                              <path
-                                d="M22 22L20 20"
-                                stroke="currentColor"
-                                strokeWidth="1.5"
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                              ></path>
-                            </svg>
-                          </span>
-                        </a>
-                      </div>
+                      </a>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
-          ))}
-        </Slider>
-      </div>
-    </>
+          </div>
+        ))}
+      </Slider>
+    </div>
+  </>
   );
 };
 
