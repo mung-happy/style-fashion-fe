f882af5ebdc79913e1678a20ff8f56996b4e3629
minhtungdoan <hin10112001@gmail.com>
2024-08-13 23:22:16 +0700

    update-layout-admin

--- src/Layout/LayoutAdmin.tsx
+++ src/Layout/LayoutAdmin.tsx
@@ -3,28 +3,76 @@ import Search from "../components/Admin/AdminMenu/Search/Search";
 import AdminMenu from "../components/Admin/AdminMenu/AdminMenu";
 import { localUserService } from "../services/localService";
 import { Outlet } from "react-router-dom";
+import { Button, Drawer, DrawerProps, Layout, RadioChangeEvent } from "antd";
+import { Content, Footer, Header } from "antd/es/layout/layout";
+import Sider from "antd/es/layout/Sider";
+import { useState } from "react";
+import { MenuFoldOutlined, MenuUnfoldOutlined } from "@ant-design/icons";
 
 function LayoutAdmin() {
   // if (localUserService.get()?.role !== "admin") {
   //   window.location.href = "/";
   // }
-  return (
-    <div className="flex min-h-screen w-full bg-bg">
-      <AdminMenu />
+  const [collapsed, setCollapsed] = useState(false);
 
-      <div className="flex-grow px-4 flex flex-col">
-        <nav className="flex items-center justify-between py-2">
-          <h6 className="font-bold capitalize text-primary">Style Fashion. admin</h6>
-          <div className="flex items-center sm:flex-row flex-col-reverse">
-            <Search className="border rounded-lg" />
-            <User />
+  const toggleCollapsed = () => {
+    setCollapsed(!collapsed);
+  };
+  const [open, setOpen] = useState(false);
+  const [placement, setPlacement] = useState<DrawerProps['placement']>('left');
+
+  const showDrawer = () => {
+    setOpen(true);
+  };
+
+  const onClose = () => {
+    setOpen(false);
+  };
+
+  const onChange = (e: RadioChangeEvent) => {
+    setPlacement(e.target.value);
+  };
+  return (
+    // <div className="flex min-h-screen w-full bg-bg admin-menu">
+    //   <AdminMenu />
+
+    //   <div className="flex-grow px-4 flex flex-col">
+    //     <nav className="flex items-center justify-between py-2">
+    //       <h6 className="font-bold capitalize text-primary">Style Fashion. admin</h6>
+    //       <div className="flex items-center sm:flex-row flex-col-reverse">
+    //         <Search className="border rounded-lg" />
+    //         <User />
+    //       </div>
+    //     </nav>
+    //     <div className="flex-grow relative flex flex-col min-w-0 mb-6 bg-white shadow-sm rounded-2xl">
+    //       <Outlet />
+    //     </div>
+    //   </div>
+    // </div>
+
+    <Layout className="flex min-h-screen w-full admin-menu">
+      <Sider className={`${collapsed ? 'block' : 'hidden'} ${!collapsed && 'lg:block hidden'} min-w-[255px]  transition-all duration-300 ease-in-out`}
+        style={{
+          backgroundColor: 'white',
+          transform: collapsed ? 'translateX(0)' : 'translateX(-100%)',
+          opacity: collapsed ? 1 : 0,
+        }}>
+        <AdminMenu collapsed={collapsed} />
+      </Sider>
+
+      <Layout>
+        <Header style={{ backgroundColor: 'white' }}>Header</Header>
+        <Content >
+          <div className="btn-collaspsed-menu">
+            <Button className="block lg:hidden " type="primary" onClick={toggleCollapsed} style={{ marginBottom: 16 }}>
+              {!collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+            </Button>
           </div>
-        </nav>
-        <div className="flex-grow relative flex flex-col min-w-0 mb-6 bg-white shadow-sm rounded-2xl">
-          <Outlet />
-        </div>
-      </div>
-    </div>
+          Content
+        </Content>
+        <Footer>Footer</Footer>
+      </Layout>
+    </Layout>
   );
 }
 
--- src/components/Admin/AdminMenu/AdminMenu.tsx
+++ src/components/Admin/AdminMenu/AdminMenu.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import imgLogo from "../../../assets/img/sf-logo2.png";
 import imgLogoIcon from "../../../assets/img/logo_icon_v2.png";
 import { MdDashboard, MdCategory } from "react-icons/md";
@@ -6,6 +6,8 @@ import { FaBoxes, FaUserAlt, FaAngleRight } from "react-icons/fa";
 import { Link, NavLink, useLocation } from "react-router-dom";
 import { BiSolidCoupon } from "react-icons/bi";
 import { FaBlog, FaTruckRampBox } from "react-icons/fa6";
+import { Button, Menu } from "antd";
+import { CaretDownOutlined, DownOutlined, MenuFoldOutlined, MenuUnfoldOutlined, UserOutlined } from '@ant-design/icons';
 
 type Menu = {
   link: string;
@@ -14,92 +16,245 @@ type Menu = {
   active: boolean;
   children?: { link: string; title: string }[];
 };
-const listMenu: Menu[] = [
+// const listMenu: Menu[] = [
+//   {
+//     link: "/",
+//     title: "Tổng quan",
+//     icon: <MdDashboard />,
+//     active: false,
+//   },
+//   {
+//     link: "/admin/categories",
+//     title: "Danh mục",
+//     icon: <MdCategory />,
+//     active: false,
+//     children: [
+//       { link: "", title: "Xem danh mục" },
+//       { link: "", title: "Thêm danh mục" },
+//     ],
+//   },
+//   {
+//     link: "/admin/products",
+//     title: "Sản phẩm",
+//     icon: <FaBoxes />,
+//     active: false,
+//     children: [
+//       { link: "", title: "Xem sản phẩm" },
+//       { link: "", title: "Thêm sản phẩm" },
+//       { link: "", title: "Quản lý đánh giá" },
+//     ],
+//   },
+//   {
+//     link: "/admin/users",
+//     title: "Người dùng",
+//     icon: <FaUserAlt />,
+//     active: false,
+//     children: [
+//       { link: "", title: "Xem người dùng" },
+//       { link: "", title: "Thêm người dùng" },
+//       { link: "", title: "Role" },
+//     ],
+//   },
+//   {
+//     link: "/admin/voucher",
+//     title: "Mã giảm giá",
+//     icon: <BiSolidCoupon />,
+//     active: false,
+//   },
+//   {
+//     link: "/admin/voucher",
+//     title: "Bài viết",
+//     icon: <FaBlog />,
+//     active: false,
+//     children: [
+//       { link: "", title: "Xem bài viết" },
+//       { link: "", title: "Thêm bài viết" },
+//     ],
+//   },
+//   {
+//     link: "/admin/voucher",
+//     title: "Đơn hàng",
+//     icon: <FaTruckRampBox />,
+//     active: false,
+//     children: [
+//       { link: "", title: "Xem đơn hàng" },
+//       { link: "", title: "Xử lý đơn hàng" },
+//       { link: "", title: "Cập nhật đơn hàng" },
+//     ],
+//   },
+// ];
+
+const menuItems = [
   {
-    link: "/",
-    title: "Tổng quan",
+    key: '1',
+    label: <Link to="/admin">Tổng quan</Link>,
     icon: <MdDashboard />,
-    active: false,
   },
   {
-    link: "/admin/categories",
-    title: "Danh mục",
+    key: '2',
+    label: 'Danh mục',
     icon: <MdCategory />,
-    active: false,
     children: [
-      { link: "", title: "Xem danh mục" },
-      { link: "", title: "Thêm danh mục" },
+      { key: '2-1', label: <Link to="/admin/categories">Xem danh mục</Link> },
+      { key: '2-2', label: <Link to="/admin/categories/add">Thêm danh mục</Link> },
     ],
   },
   {
-    link: "/admin/products",
-    title: "Sản phẩm",
+    key: '3',
+    label: 'Sản phẩm',
     icon: <FaBoxes />,
-    active: false,
     children: [
-      { link: "", title: "Xem sản phẩm" },
-      { link: "", title: "Thêm sản phẩm" },
-      { link: "", title: "Quản lý đánh giá" },
+      { key: '3-1', label: <Link to="/admin/products">Xem sản phẩm</Link> },
+      { key: '3-2', label: <Link to="/admin/products/add">Thêm sản phẩm</Link> },
+      { key: '3-3', label: <Link to="/admin/products">Quản lý đánh giá</Link> },
     ],
   },
   {
-    link: "/admin/users",
-    title: "Người dùng",
-    icon: <FaUserAlt />,
-    active: false,
+    key: '4',
+    label: 'Người dùng',
+    icon: <UserOutlined />,
     children: [
-      { link: "", title: "Xem người dùng" },
-      { link: "", title: "Thêm người dùng" },
-      { link: "", title: "Role" },
+      { key: '4-1', label: <Link to="/admin/users">Xem người dùng</Link> },
+      { key: '4-2', label: <Link to="/admin/users/add">Thêm người dùng</Link> },
+      { key: '4-3', label: <Link to="/admin/users">Role</Link> },
     ],
   },
   {
-    link: "/admin/voucher",
-    title: "Mã giảm giá",
+    key: '5',
+    label: <Link to="/admin/voucher">Mã giảm giá</Link>,
     icon: <BiSolidCoupon />,
-    active: false,
   },
   {
-    link: "/admin/voucher",
-    title: "Bài viết",
+    key: '6',
+    label: 'Bài viết',
     icon: <FaBlog />,
-    active: false,
     children: [
-      { link: "", title: "Xem bài viết" },
-      { link: "", title: "Thêm bài viết" },
+      { key: '6-1', label: <Link to="/admin">Xem bài viết</Link> },
+      { key: '6-2', label: <Link to="/admin">Thêm bài viết</Link> },
     ],
   },
   {
-    link: "/admin/voucher",
-    title: "Đơn hàng",
+    key: '7',
+    label: 'Đơn hàng',
     icon: <FaTruckRampBox />,
-    active: false,
     children: [
-      { link: "", title: "Xem đơn hàng" },
-      { link: "", title: "Xử lý đơn hàng" },
-      { link: "", title: "Cập nhật đơn hàng" },
+      { key: '7-1', label: <Link to="/admin/order">Xem đơn hàng</Link> },
+      { key: '7-2', label: <Link to="/admin/order">Xử lý đơn hàng</Link> },
+      { key: '7-3', label: <Link to="/admin/order">Cập nhật đơn hàng</Link> },
     ],
   },
 ];
 
-const AdminMenu: React.FC = () => {
+type Props = {
+  collapsed: boolean;
+};
+
+const AdminMenu: React.FC<Props> = ({ collapsed }): any => {
   const location = useLocation();
   const fullPath = location.pathname;
   const pathSegments = fullPath.split("/").slice(1, 3); // Chỉ lấy 'admin' và 'products'
   const pathAfterAdmin = "/" + pathSegments.join("/");
 
+
   return (
-    <div className="lg:w-52 lg:px-4 pl-4 lg:min-w-[200px] duration-500">
-      <div className="h-full w-8 lg:w-full">
+    // <div className="lg:w-[255px] lg:px-4 pl-4  duration-500">
+    //   <div className="h-full w-8 lg:w-full">
+    //     <Link
+    //       className="block lg:px-8 lg:py-6 my-6 text-sm text-slate-700"
+    //       to="/admin/products"
+    //     >
+    //       <img src={imgLogo} className="hidden lg:block" />
+    //       <img src={imgLogoIcon} className="lg:hidden inline-block" />
+    //     </Link>
+    //     {/* <hr className="h-px border-0 bg-transparent bg-gradient-to-r from-transparent via-black/40 to-transparent" /> */}
+    //     {/* <div className="mt-4">
+    //       <ul className="flex flex-col">
+    //         {listMenu.map(({ link, title, icon, active, children }, index) => {
+    //           return (
+    //             <li
+    //               key={index}
+    //               className="relative group hover:bg-gray-200 duration-300 rounded-lg"
+    //             >
+    //               <NavLink
+    //                 className={`text-sm flex items-center lg:px-2 py-2.5 ${
+    //                   pathAfterAdmin === link ? "lg:bg-white lg:shadow-xl" : ""
+    //                 } `}
+    //                 to={link}
+    //               >
+    //                 <div
+    //                   className={`lg:mr-2 flex h-8 w-8 items-center justify-center rounded-lg ${
+    //                     active
+    //                       ? "lg:bg-transparent lg:shadow-none bg-white shadow-lg"
+    //                       : ""
+    //                   } `}
+    //                 >
+    //                   {icon}
+    //                 </div>
+    //                 <div className="hidden justify-between items-center flex-grow opacity-100 lg:flex">
+    //                   <span className="ml-1">{title}</span>
+    //                   {children && <FaAngleRight />}
+    //                 </div>
+    //                 {children ? (
+    //                   <div className="absolute z-10 left-full -top-1 invisible w-40 bg-white opacity-0 group-hover:opacity-100 group-hover:visible duration-300">
+    //                     <div className="bg-white m-2 p-1 shadow-[rgba(27,31,35,0.15)_0px_0px_0px_1px] rounded-lg w-full">
+    //                       <span className="font-semibold text-gray-400 p-2">
+    //                         {title}
+    //                       </span>
+    //                       {children?.map((child) => (
+    //                         <Link
+    //                           className="text-sm p-2 rounded-md hover:bg-[#e7e7e7] w-full block"
+    //                           to={child.link}
+    //                         >
+    //                           {child.title}
+    //                         </Link>
+    //                       ))}
+    //                     </div>
+    //                     <span className="absolute border-[8px] border-t-transparent border-b-transparent border-l-transparent top-5 -left-2"></span>
+    //                   </div>
+    //                 ) : (
+    //                   <div className="absolute z-10 left-full -top-1 invisible w-40 bg-white opacity-0 group-hover:visible duration-300 group-hover:opacity-100 lg:hidden cursor-text">
+    //                     <div className="bg-white m-2 p-1 shadow-[rgba(27,31,35,0.15)_0px_0px_0px_1px] rounded-lg w-full">
+    //                       <p className="font-semibold p-2">{title}</p>
+    //                     </div>
+    //                     <span className="absolute border-[8px] border-t-transparent border-b-transparent border-l-transparent top-5 -left-2"></span>
+    //                   </div>
+    //                 )}
+    //               </NavLink>
+    //             </li>
+    //           );
+    //         })}
+    //       </ul>
+    //     </div> */}
+    //     <div className="">
+    //       <Button className="block lg:hidden " type="primary" onClick={toggleCollapsed} style={{ marginBottom: 16 }}>
+    //         {collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+    //       </Button>
+    //     </div>
+    //     <div className={`${collapsed ? 'block' : 'hidden'} lg:block`}>
+    //       <Menu
+    //         // onClick={onClick}
+    //         style={{ width: 256 }}
+    //         // defaultSelectedKeys={['3-1']}
+    //         defaultOpenKeys={['1']}
+    //         mode="inline"
+    //         items={menuItems}
+    //         inlineCollapsed={collapsed}
+    //       // expandIcon={({ isOpen }) => <CaretDownOutlined rotate={isOpen ? 180 : 0} />}
+    //       />
+    //     </div>
+    //   </div>
+    // </div>
+    <div className="">
+      <div className="">
         <Link
           className="block lg:px-8 lg:py-6 my-6 text-sm text-slate-700"
           to="/admin/products"
         >
           <img src={imgLogo} className="hidden lg:block" />
-          <img src={imgLogoIcon} className="lg:hidden inline-block" />
+          {/* <img src={imgLogoIcon} className="lg:hidden inline-block" /> */}
         </Link>
-        <hr className="h-px border-0 bg-transparent bg-gradient-to-r from-transparent via-black/40 to-transparent" />
-        <div className="mt-4">
+        {/* <hr className="h-px border-0 bg-transparent bg-gradient-to-r from-transparent via-black/40 to-transparent" /> */}
+        {/* <div className="mt-4">
           <ul className="flex flex-col">
             {listMenu.map(({ link, title, icon, active, children }, index) => {
               return (
@@ -156,6 +311,19 @@ const AdminMenu: React.FC = () => {
               );
             })}
           </ul>
+        </div> */}
+
+        <div>
+          <Menu
+            // onClick={onClick}
+            // style={{ width: 256 }}
+            // defaultSelectedKeys={['3-1']}
+            defaultOpenKeys={['1']}
+            mode="inline"
+            items={menuItems}
+            inlineCollapsed={collapsed}
+          // expandIcon={({ isOpen }) => <CaretDownOutlined rotate={isOpen ? 180 : 0} />}
+          />
         </div>
       </div>
     </div>
